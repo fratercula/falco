496e709ce45d6c1ac112327953296a8d340e333e
LoeiFy <LoeiFy@gmail.com>
2019-01-20 20:09:24 +0800

    build

--- test/index.js
+++ test/index.js
@@ -1,8 +1,13 @@
 const falco = require('../index')
 
-const js = `import React from 'react'
-import { render } from 'react-dom'
-render(<div>hello world</div>, document.querySelector('#root'))`
+// const js = `import React from 'react'
+// import { render } from 'react-dom'
+// render(<div>hello world</div>, document.querySelector('#root'))`
 
-falco({ entry: { js } })
+const js1 = `import two from 'two'
+console.log(two())`
+
+falco({
+  entry: [{ js: js1 }],
+})
   .then(res => console.log(res))

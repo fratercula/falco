d3c9b00ab68cd9b43b9d5de6b417264ce5056c91
LoeiFy <LoeiFy@gmail.com>
2019-01-22 23:57:34 +0800

    require modules

--- test/index.js
+++ test/index.js
@@ -7,7 +7,12 @@ const falco = require('../')
 const js1 = `import two from 'two'
 console.log(two())`
 
+const js2 = 'console.log(\'?\')'
+
+const js3 = `const two = require('two')
+console.log(two())`
+
 falco({
-  entry: [{ js: js1 }],
+  entry: [{ js: js3 }],
 })
   .then(res => console.log(res))
